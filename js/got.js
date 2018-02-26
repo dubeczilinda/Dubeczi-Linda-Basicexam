8e550f1e532daf06a388569e0c789ed3b4a724cf
Linda Dubeczi <dublimi5@yahoo.com>
2018-02-26 16:20:40 +0100

    That's all

--- js/got.js
+++ js/got.js
@@ -22,10 +22,45 @@ function successAjax(xhttp) {
       A userDatas NEM GLOBÁLIS változó, ne is tegyétek ki globálisra. Azaz TILOS!
       Ha valemelyik függvényeteknek kell, akkor paraméterként adjátok át.
     */
+    getNames(userDatas);
+    createDiv(userDatas);
 }
 
 // Írd be a json fileod nevét/útvonalát úgy, ahogy nálad van
-getData('/json/aJsonFileodNeve.json', successAjax);
+getData('/json/characters.json', successAjax);
 
 // Live servert használd mindig!!!!!
 /* IDE ÍRD A FÜGGVÉNYEKET!!!!!! NE EBBE AZ EGY SORBA HANEM INNEN LEFELÉ! */
+
+function getNames() {
+    for (var i = 0; i < userDatas.length; i++){
+        pNames = document.createElement('p');
+        if(userDatas[i].dead === "") {
+            pNames.innerHTML = userDatas[i].name;
+            document.querySelector("#map").appendChild(pNames);
+        }
+    }
+}
+
+function createDiv() {
+        for (var i = 0; i < userDatas.length; i++) {
+            divPortrait = document.createElement('div');
+            if(userDatas[i].dead === "") {
+            divPortrait.innerHTML = userDatas[i].portrait;
+            
+        }
+    }
+}
+
+// keresés
+function searchCharacter(){
+    for (var i = 0; i < userDatas.length; i++){
+        var targetCharacter;
+        if(parse(document.getElementById("#search").innerHTML) == userDatas[i].name){
+            targetCharacter = userDatas[i].name;
+        }
+        else {
+            targetCharacter.innerHTML = 'Character not found';
+        }
+    }
+}
